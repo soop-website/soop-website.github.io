b57ddcafe635af3a55b92e74cc3b8e8bb4cfdd59
yurikasper <yurikasper14@protonmail.com>
2025-03-16 05:42:05 -0300

    fix: move static files to static folder

--- placeholder.js
+++
@@ -1,8 +0,0 @@
-var colors = ['#C32026', '#E3C828','#5AB449','#29ACA7','#2495CE','#D11E85','#7A2B88'];
-var i = 0;
-
-setInterval(() => {
-    document.body.style.backgroundColor = colors[i];
-    i++
-    if(i == colors.length) i = 0;
-}, 800);
